3aa63b228c5fc62e6d08aa8af58f81e9ae47b7de
lizhongl <907678041@qq.com>
2018-01-22 15:54:14 +0800

    测试合并

--- 0123/src/main/java/Account.java
+++ 0123/src/main/java/Account.java
@@ -3,6 +3,23 @@
  */
 public class Account {
 
-    public Account() {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
